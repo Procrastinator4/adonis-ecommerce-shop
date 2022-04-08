5da08a4488562e77f5e33dbd4d53c0d878d58baa
Elliot Alderson <elliotalderson4444@gmail.com>
2022-04-08 12:00:47 +0500

    fix the animation issue in products page

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -7,13 +7,12 @@ const Products = () => {
         <h1 className="font-black my-8 text-4xl  text-[#383737]">
           PRODUCT OVERVIEW
         </h1>
-        <div className="grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:gap-4 md:gap-8 lg:gap-12  ">
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 1] }}
-            transition={{ duration: 0.5, delay: 1 }}
-            className="overflow-hidden rounded-xl"
-          >
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: [0, 1] }}
+          className="grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:gap-4 md:gap-8 lg:gap-12  "
+        >
+          <div className="overflow-hidden rounded-xl">
             <img
               className="rounded-lg transform hover:scale-105 transition duration-250 ease-in-out overflow-hidden"
               src="/po1g.webp"
@@ -21,13 +20,8 @@ const Products = () => {
             />
             <h4 className="text-slate-500 my-2">Esprit Ruffle Shirt</h4>
             <p className="text-slate-800">$16</p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 1] }}
-            transition={{ duration: 0.5, delay: 1 }}
-            className="overflow-hidden rounded-xl"
-          >
+          </div>
+          <div className="overflow-hidden rounded-xl">
             <img
               className="rounded-lg transform hover:scale-105 transition duration-250 ease-in-out"
               src="/po2g.webp"
@@ -35,13 +29,8 @@ const Products = () => {
             />
             <h4 className="text-slate-500 my-2">Herschel supply</h4>
             <p className="text-slate-800">$46</p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 1] }}
-            transition={{ duration: 0.5, delay: 1 }}
-            className="overflow-hidden rounded-xl"
-          >
+          </div>
+          <div className="overflow-hidden rounded-xl">
             <img
               className="rounded-lg transform hover:scale-105 transition duration-250 ease-in-out"
               src="/po3b.webp"
@@ -49,13 +38,8 @@ const Products = () => {
             />
             <h4 className="text-slate-500 my-2">Only Check Trouser</h4>
             <p className="text-slate-800">$26</p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 1] }}
-            transition={{ duration: 0.5, delay: 1 }}
-            className="overflow-hidden rounded-xl"
-          >
+          </div>
+          <div className="overflow-hidden rounded-xl">
             <img
               className="rounded-lg transform hover:scale-105 transition duration-250 ease-in-out"
               src="/po4g.webp"
@@ -63,13 +47,8 @@ const Products = () => {
             />
             <h4 className="text-slate-500 my-2">Front Pocket Jumper</h4>
             <p className="text-slate-800">$34.5</p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 1] }}
-            transition={{ duration: 0.5, delay: 1 }}
-            className="overflow-hidden rounded-xl"
-          >
+          </div>
+          <div className="overflow-hidden rounded-xl">
             <img
               className="rounded-lg transform hover:scale-105 transition duration-250 ease-in-out"
               src="/po5g.webp"
@@ -77,13 +56,8 @@ const Products = () => {
             />
             <h4 className="text-slate-500 my-2">Vintage Inspired Classic</h4>
             <p className="text-slate-800">$56</p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 1] }}
-            transition={{ duration: 0.5, delay: 1 }}
-            className="overflow-hidden rounded-xl"
-          >
+          </div>
+          <div className="overflow-hidden rounded-xl">
             <img
               className="rounded-lg transform hover:scale-105 transition duration-250 ease-in-out"
               src="/po6watch.webp"
@@ -91,13 +65,8 @@ const Products = () => {
             />
             <h4 className="text-slate-500 my-2">Pieces Metallic Printed</h4>
             <p className="text-slate-800">$5.34</p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 1] }}
-            transition={{ duration: 0.5, delay: 1 }}
-            className="overflow-hidden rounded-xl"
-          >
+          </div>
+          <div className="overflow-hidden rounded-xl">
             <img
               className="rounded-lg transform hover:scale-105 transition duration-250 ease-in-out"
               src="/po7g.jpg"
@@ -107,13 +76,8 @@ const Products = () => {
               Converse All Star Hi Plimsolls
             </h4>
             <p className="text-slate-800">$12</p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 1] }}
-            transition={{ duration: 0.5, delay: 1 }}
-            className="overflow-hidden rounded-xl"
-          >
+          </div>
+          <div className="overflow-hidden rounded-xl">
             <img
               className="rounded-lg transform hover:scale-105 transition duration-250 ease-in-out"
               src="/po8g.webp"
@@ -121,13 +85,8 @@ const Products = () => {
             />
             <h4 className="text-slate-500 my-2">Femme T-Shirt In Stripe</h4>
             <p className="text-slate-800">$23.5</p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 1] }}
-            transition={{ duration: 0.5, delay: 1 }}
-            className="overflow-hidden rounded-xl"
-          >
+          </div>
+          <div className="overflow-hidden rounded-xl">
             <img
               className="rounded-lg transform hover:scale-105 transition duration-250 ease-in-out"
               src="/po9boot.webp"
@@ -135,13 +94,8 @@ const Products = () => {
             />
             <h4 className="text-slate-500 my-2">Herschel supply</h4>
             <p className="text-slate-800">$25.5</p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 1] }}
-            transition={{ duration: 0.5, delay: 1 }}
-            className="overflow-hidden rounded-xl"
-          >
+          </div>
+          <div className="overflow-hidden rounded-xl">
             <img
               className="rounded-lg transform hover:scale-105 transition duration-250 ease-in-out"
               src="/po10g.webp"
@@ -149,13 +103,8 @@ const Products = () => {
             />
             <h4 className="text-slate-500 my-2">Herschel supply</h4>
             <p className="text-slate-800">$40</p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 1] }}
-            transition={{ duration: 0.5, delay: 1 }}
-            className="overflow-hidden rounded-xl"
-          >
+          </div>
+          <div className="overflow-hidden rounded-xl">
             <img
               className="rounded-lg transform hover:scale-105 transition duration-250 ease-in-out"
               src="/po11b.webp"
@@ -163,13 +112,8 @@ const Products = () => {
             />
             <h4 className="text-slate-500 my-2">T-Shirt with Sleeve</h4>
             <p className="text-slate-800">$13</p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 1] }}
-            transition={{ duration: 0.5, delay: 1 }}
-            className="overflow-hidden rounded-xl"
-          >
+          </div>
+          <div className="overflow-hidden rounded-xl">
             <img
               className="rounded-lg transform hover:scale-105 transition duration-250 ease-in-out"
               src="/po12belt.webp"
@@ -177,13 +121,8 @@ const Products = () => {
             />
             <h4 className="text-slate-500 my-2">Pretty Little Thing</h4>
             <p className="text-slate-800">$19</p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 1] }}
-            transition={{ duration: 0.5, delay: 1 }}
-            className="overflow-hidden rounded-xl"
-          >
+          </div>
+          <div className="overflow-hidden rounded-xl">
             <img
               className="rounded-lg transform hover:scale-105 transition duration-250 ease-in-out"
               src="/po13g.webp"
@@ -191,13 +130,8 @@ const Products = () => {
             />
             <h4 className="text-slate-500 my-2">Mini Silver Mesh Watch</h4>
             <p className="text-slate-800">$31.5</p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 1] }}
-            transition={{ duration: 0.5, delay: 1 }}
-            className="overflow-hidden rounded-xl"
-          >
+          </div>
+          <div className="overflow-hidden rounded-xl">
             <img
               className="rounded-lg transform hover:scale-105 transition duration-250 ease-in-out"
               src="/po14shirt.webp"
@@ -205,13 +139,8 @@ const Products = () => {
             />
             <h4 className="text-slate-500 my-2">Square Neck Back</h4>
             <p className="text-slate-800">$50</p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 1] }}
-            transition={{ duration: 0.5, delay: 1 }}
-            className="overflow-hidden rounded-xl"
-          >
+          </div>
+          <div className="overflow-hidden rounded-xl">
             <img
               className="rounded-lg transform hover:scale-105 transition duration-250 ease-in-out"
               src="/po15watch2.webp"
@@ -219,13 +148,8 @@ const Products = () => {
             />
             <h4 className="text-slate-500 my-2">Square Neck Back</h4>
             <p className="text-slate-800">$50</p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 1] }}
-            transition={{ duration: 0.5, delay: 1 }}
-            className="overflow-hidden rounded-xl"
-          >
+          </div>
+          <div className="overflow-hidden rounded-xl">
             <img
               className="rounded-lg transform hover:scale-105 transition duration-250 ease-in-out"
               src="/po16g.webp"
@@ -233,8 +157,8 @@ const Products = () => {
             />
             <h4 className="text-slate-500 my-2">Square Neck Back</h4>
             <p className="text-slate-800">$50</p>
-          </motion.div>
-        </div>
+          </div>
+        </motion.div>
       </div>
     </section>
   );
